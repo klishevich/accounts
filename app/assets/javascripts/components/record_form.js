7e612f854ba377f101843bd1597b8f7b663f27d5
klishevich <busation@gmail.com>
2016-02-14 15:28:46 +0300

    coffee to JS

---
+++ app/assets/javascripts/components/record_form.js
@@ -0,0 +1,74 @@
+(function() {
+  this.RecordForm = React.createClass({
+    getInitialState: function() {
+      return {
+        title: '',
+        date: '',
+        amount: ''
+      };
+    },
+    handleChange: function(e) {
+      var name, obj;
+      name = e.target.name;
+      return this.setState((
+        obj = {},
+        obj["" + name] = e.target.value,
+        obj
+      ));
+    },
+    valid: function() {
+      return this.state.title && this.state.date && this.state.amount;
+    },
+    handleSubmit: function(e) {
+      e.preventDefault();
+      return $.post('', {
+        record: this.state
+      }, (function(_this) {
+        return function(data) {
+          _this.props.handleNewRecord(data);
+          return _this.setState(_this.getInitialState());
+        };
+      })(this), 'JSON');
+    },
+    render: function() {
+      return React.DOM.form({
+        className: 'form-inline',
+        onSubmit: this.handleSubmit
+      }, React.DOM.h2({
+        className: 'none'
+      }, "Test!!!"), React.DOM.div({
+        className: 'form-group'
+      }, React.DOM.input({
+        type: 'text',
+        className: 'form-control',
+        placeholder: 'Date',
+        name: 'date',
+        value: this.state.date,
+        onChange: this.handleChange
+      })), React.DOM.div({
+        className: 'form-group'
+      }, React.DOM.input({
+        type: 'text',
+        className: 'form-control',
+        placeholder: 'Title',
+        name: 'title',
+        value: this.state.title,
+        onChange: this.handleChange
+      })), React.DOM.div({
+        className: 'form-group'
+      }, React.DOM.input({
+        type: 'number',
+        className: 'form-control',
+        placeholder: 'Amount',
+        name: 'amount',
+        value: this.state.amount,
+        onChange: this.handleChange
+      })), React.DOM.button({
+        type: 'submit',
+        className: 'btn btn-primary',
+        disabled: !this.valid()
+      }, 'Create record'));
+    }
+  });
+
+}).call(this);
